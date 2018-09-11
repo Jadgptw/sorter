1c84c19035b8d323a74fc73ca7f579f16669e7fa
Jadgptw <Gordin.Alexander90@gmail.com>
2018-09-12 02:16:31 +0300

    Implimentation without comparator

--- src/index.js
+++ src/index.js
@@ -1,26 +1,47 @@
 class Sorter {
   constructor() {
     // your implementation
+      this.array = [];
   }
 
   add(element) {
     // your implementation
+      this.array.push(element);
   }
 
   at(index) {
     // your implementation
+      return this.array[index];
   }
 
   get length() {
     // your implementation
+      return this.array.length;
   }
 
   toArray() {
     // your implementation
+      return this.array;
   }
 
   sort(indices) {
     // your implementation
+      let arr = [];
+      indices.sort();
+      // console.log("Массив отсортированных индексов:" + indices);
+      for(let i = 0; i <= indices.length - 1; i++) {
+        arr.push(this.array[indices[i]]);
+      }
+      // console.log("Массив, подлежащий сортировке: " + arr);
+      arr.sort(function (a, b) {
+        return a - b;
+      });
+      // console.log("Отсортированный масив: " + arr);
+      for(let i = 0; i <= indices.length - 1; i++) {
+          this.array[indices[i]] = arr[i];
+      }
+      // console.log("Результат: " + this.array);
+      // console.log("Размер массива: " + this.array.length);
   }
 
   setComparator(compareFunction) {
